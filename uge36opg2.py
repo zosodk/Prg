c3007696ae03a6e968f9823ba17d1153489d0b7d
Klaus Hviid <zosodk@icloud.com>
2026-09-03 11:37:37 +0200

    Opgaver lavet

--- uge36opg2.py
+++ uge36opg2.py
@@ -1,3 +1,3 @@
 print("Hej med dig\n")
-name = input("Hvad hedder du? ")
-print("Hej " + name)
+name = input("Hvad hedder du?\n")
+print("Hej " + name.capitalize() + "\nRart at møde dig!")
--- uge36opg3.py
+++ uge36opg3.py
@@ -1,3 +1,10 @@
-print("Hej med dig\n")
-name = input("Hvad hedder du? ")
-print("Hej " + name)
+print("Hejsa\nJeg skal bruge to tal, fra dig\n")
+tal1 = input("Tal nr 1: ")
+tal2 = input("Tal nr 2: ")
+print("Summen er: " + str(int(tal1) + int(tal2)) + "\n")
+print("Forskellen er: " + str(int(tal1) - int(tal2)) + "\n")
+print("Produktet er: " + str(int(tal1) * int(tal2)) + "\n")
+print("Divisionen er: " + str(int(tal1) / int(tal2)) + "\n")
+#Modulus er en heltalsdivision, der undersøger hvor mange gange tal2 går op i tal1 og returnerer resten
+print("Modulus er: " + str(int(tal1) % int(tal2)) + "\n")
+
