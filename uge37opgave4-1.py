497e42623a053b41091f9c5e2a7acd60c7bd46cc
Klaus Hviid <zosodk@icloud.com>
2026-09-07 16:36:35 +0200

    Opgaver til kapitel 4

---
+++ uge37opgave4-1.py
@@ -0,0 +1,5 @@
+pizzas = ["Margerite","Pepporoni","Vegan"]
+for pizza in pizzas:
+    print(f"I like {pizza}")
+print(f"I really like {pizzas[0]}, but I love {pizzas[1]} the most.\n")
+print(f"I really love pizza!")
---
+++ uge37opgave4-2.py
@@ -0,0 +1,4 @@
+animals = ["cat", "dog", "parrot", "ferret"]
+for animal in animals:
+    print(f"{animal}s are cute!")
+print(f"Animals are almost always cute, but {animals[0]}s are the most cute!")
