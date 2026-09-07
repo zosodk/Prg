317008850b7d9a855fc531cb11cd0b8c9c82163f
Klaus Hviid <zosodk@icloud.com>
2026-09-07 12:05:50 +0200

    Uge 37 Opgaver på klassen

---
+++ uge37opg2.py
@@ -0,0 +1,6 @@
+#Lister i python
+names=[1,2,3,4]
+names += ["A","B","C","D"]
+#print(names[7])
+names[4] += "llan"
+print(names[4])
---
+++ uge37opg3-8.py
@@ -0,0 +1,25 @@
+countries = ["Germany", "France", "Italy","Spain","Portugal"]
+while len(countries) > 0:
+    #print(countries[0:3])
+    print("Lande soom indskrevet på listen: ")
+    print(countries)
+    print("Lande sorteret i udskrift:")
+    print(sorted(countries))
+    print("Lande som indskrevet på listen:")
+    print(countries)
+    print("Lande i listen i omvendt orden: ")
+    countries.reverse()
+    print(countries)
+    print("Lande i listen i korrekt orden igen: ")
+    countries.reverse()
+    print(countries)
+    print("Lande alfabetisk sorteret: ")
+    countries.sort()
+    print(countries)
+    print("Lande omvendt alfabetisk sorteret: ")
+    countries.sort(reverse=True)
+    print(countries)
+
+    break
+else :
+    print("Ingen lande i listen!")
