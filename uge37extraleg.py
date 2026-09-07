9260150c96483e9067d8244db03a72599a57bf17
Klaus Hviid <zosodk@icloud.com>
2026-09-07 16:23:44 +0200

    fiksede lige en fejl fra mit keyboard

--- uge37extraleg.py
+++ uge37extraleg.py
@@ -4,4 +4,4 @@ with open("uge37extraleg.txt", 'w') as fil:
     fil.close()
 with open("uge37extraleg.txt", 'r') as fil:
     linjer = [linje.strip() for linje in fil.readlines()]
-print(linjer)s
+print(linjer)
