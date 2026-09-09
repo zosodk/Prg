cbcf99127ce430f09181f6fbe52c83f1c89fc89c
Klaus Hviid <zosodk@icloud.com>
2026-09-10 00:49:09 +0200

    Optimering af kode med elementer fra kapitel 4

--- Uge37/uge37opg1.py
+++ Uge37/uge37opg1.py
@@ -1,40 +1,28 @@
 #Opgaver på klassen
 #En regning på en restaurant skal deles i tre lige store dele inkl drikkepenge.
-#Skriv Total
-#Skriv drikkepenge på 15%
-#Skriv Totalen
-#Hver del personerne skal af med.
-"""totalnette = float()
-drikkepenge = float(0.15)
-antalpersoner = int
-total = float
-drikkepenge+=1
-total = totalnette * drikkepenge
-anpart = total / antalpersoner
-"""
+# Hent input og konverter med det samme
+people = int(input("Hvor mange personer er I?: "))
+total_price = float(input("Hvad er den samlede pris?: "))
+percent_of_tip = float(input("Indtast drikkepenge i procent (%): "))
 
-guest1 = input("Indtast navnet på den første gæst?")
-#debug
-print("Første gæst hedder", guest1)
-guest2 = input("Indtast navnet på den anden gæst?")
-#debug
-print("Anden gæst hedder", guest2)
-guest3 = input("Indtast navnet på den tredje gæst?")
-#debug
-print("Tredje gæst hedder", guest3)
-bill = input("Indtast regningens total?")
-bill = float(bill)
-print("Regningen er", bill)
-print("Indtast drikkepengene som procent")
-drikp = input("Drikkepenge % er")
-drikp = int(drikp)
-print("Drikkepenge i procenter er", drikp)
-tip = bill * (drikp/100)
-print("Drikkepengene udgør dette af regningr", tip)
-total_amount = bill + tip
-print("Regningen er", total_amount)
-guest_amount = total_amount / 3
-print("Hver gæst skal betale", round(guest_amount, 2))
+# Beregn det hele
+amount_to_tip = total_price * (percent_of_tip / 100)
+total_inc_tip = total_price + amount_to_tip
+tip_per_guest = amount_to_tip / people
+per_guest = total_inc_tip / people
+
+# Byg listen med input og en for løkke med range på antallet af gæster
+names = [input(f"Indtast navnet på gæst {i+1}: ") for i in range(people)]
+
+# Udskriv resultater med 2 decimaler
+print(f"\nRegningen lyder på: {total_price:.2f} kr.")
+print(f"Samlede drikkepenge: {amount_to_tip:.2f} kr.")
+print(f"Drikkepenge pr. gæst: {tip_per_guest:.2f} kr.")
+print(f"Samlet beløb inkl. drikkepenge: {total_inc_tip:.2f} kr.\n")
+
+#Skriv beløb pr navn
+for name in names:
+    print(f"{name} skal betale: {per_guest:.2f} kr.")
 
 
 
