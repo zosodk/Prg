a112a8d6c18af18652f545110321b1ff89c8bddf
Klaus Hviid <zosodk@icloud.com>
2026-09-14 10:40:49 +0200

    Uge 38 Opgaver på klassen kapitel 5

---
+++ Uge38/uge38opg1.py
@@ -0,0 +1,10 @@
+# Opgave 5.3, 5.4, 5.5
+alien_color = "blue"
+print("You have been spotted!")
+if alien_color == "green":
+    print("You have earned 5 points!")
+elif alien_color == "yellow":
+    print("You have earned 10 points!")
+else:
+    print("You have earned 15 points!")
+print("The End!")
